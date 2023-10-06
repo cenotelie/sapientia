d017c2aeff162d7cd76e302651f899af4f888d22
Stella <stella@stellaires>
2023-10-06 17:25:24 +0200

    Relation extraction

--- sapientia/nlp/components/rel_heuristic/relation_extraction.py
+++ sapientia/nlp/components/rel_heuristic/relation_extraction.py
@@ -72,6 +72,47 @@ def get_sentence_for_named_entity(named_entity, text):
     return sentences[sentence_beginning]
 
 
+def get_sentence_start(sentence, sentences):
+    """
+    Get sentence start character index
+    :param sentence: sentence
+    :param sentences: sentences of the text (dict associating start characters and sentences)
+    :return: sentence start
+    """
+    sentence_start = 0
+    if not isinstance(sentences, dict):
+        raise TypeError(sentences, " invalid sentences")
+    for start, s in sentences.items():
+        if s == sentence:
+            sentence_start = start
+    return sentence_start
+
+
+def get_sentence_end(sentence, sentence_start):
+    """
+    Get sentence end character
+    :param sentence: sentence
+    :param sentence_start: sentence start character index
+    :return: sentence end
+    """
+    return sentence_start + len(sentence)
+
+
+def is_in_sentence(named_entity, sentence_start, sentence_end):
+    """
+    Check if a named entity is in a sentence
+    :param named_entity: named entity
+    :param sentence_start: sentence start
+    :param sentence_end: sentence end
+    :return: boolean, true if named entity is in the sentence, false otherwise
+    """
+    if not isinstance(named_entity, dict):
+        raise TypeError(named_entity, " invalid named entity")
+    if "start_char" not in named_entity.keys():
+        raise TypeError(named_entity, " invalid named entity")
+    return sentence_start < named_entity["start_char"] < sentence_end
+
+
 def get_ner_labels(named_entities):
     """
     Get named entities labels from named entities
@@ -165,23 +206,82 @@ def definition_extraction(named_entities, text):
     for named_entity in named_entities:
         if named_entity["label"] == "DOCUMENT":
             documents.append(named_entity)
+    sentences = sentence_segmentation(text)
     for document in documents:
         sentence = get_sentence_for_named_entity(document, text)
+        sentence_start = get_sentence_start(sentence, sentences)
+        sentence_end = get_sentence_end(sentence, sentence_start)
+        for named_entity in named_entities:
+            if named_entity["label"] == "STANDARD" or named_entity["label"] == "PROCESS":
+                if is_in_sentence(named_entity, sentence_start, sentence_end):
+                    is_definition = False
+                    if named_entity["start_char"] < document["start_char"] and "defined" in sentence:
+                        # Text example : "the requirements are defined in the document"
+                        # Text example : "the purchaser will provide a document in which the requirement are defined"
+                        # Text example : "the purchaser has defined the requirements in the document"
+                        is_definition = True
+                    if document["start_char"] > named_entity["start_char"] and ("define" or "defines") in sentence:
+                        define_index = get_sentence_start(sentence, sentences) + sentence.find("define")
+                        if document["start_char"] < define_index < named_entity["start_char"] or \
+                                define_index < named_entity["start_char"] < document["start_char"]:
+                            # Text example : "The document defines the requirements"
+                            # Text example : "The supplier shall define requirements in the document"
+                            is_definition = True
+                    if is_definition:
+                        definition = "DEFINED_BY(" + named_entity["text"].lower() + "," + document["text"].lower() + ")"
+                        if definition not in definition_relations:
+                            definition_relations.append(definition)
     return definition_relations
 
 
-def compliance_extraction():
+def compliance_extraction(named_entities, text):
     """
-
+    COMPLY_WITH relation extraction (heuristic method)
+    :param named_entities: named entities
+    :param text: text
+    :return: comply with relations
     """
-    return []
+    compliance_relations = []
+    standards_and_criteria = []
+    for named_entity in named_entities:
+        if named_entity["label"] == "STANDARD" or named_entity["label"] == "CRITERIA":
+            standards_and_criteria.append(named_entity)
+    sentences = sentence_segmentation(text)
+    for standard_and_criteria in standards_and_criteria:
+        sentence = get_sentence_for_named_entity(standard_and_criteria, text)
+        sentence_start = get_sentence_start(sentence, sentences)
+        sentence_end = get_sentence_end(sentence, sentence_start)
+        for named_entity in named_entities:
+            if named_entity["label"] == "DOCUMENT" or named_entity["label"] == "ROLE" or named_entity["label"] == "ORG":
+                if is_in_sentence(named_entity, sentence_start, sentence_end):
+                    is_compliance = False
+                    if named_entity["start_char"] < standard_and_criteria["start_char"] and ("comply" in sentence or
+                                                                                             "complies" in sentence):
+                        compliance_index = 0
+                        if "comply" in sentence:
+                            compliance_index = get_sentence_start(sentence, sentences) + sentence.find("comply")
+                        if "complies" in sentence:
+                            compliance_index = get_sentence_start(sentence, sentences) + sentence.find("complies")
+                        if named_entity["start_char"] < compliance_index < standard_and_criteria["start_char"]:
+                            is_compliance = True
+                        # Text example : "the purchaser complies with the requirements defined in the document"
+                    if is_compliance:
+                        compliance = ("COMPLY_WITH(" + named_entity["text"].lower() + "," +
+                                      standard_and_criteria["text"].lower() + ")")
+                        if compliance not in compliance_relations:
+                            compliance_relations.append(compliance)
+    return compliance_relations
 
 
-def composition_extraction():
+def composition_extraction(named_entities, text):
     """
-
+    COMPOSED_BY relation extraction (heuristic method)
+    :param named_entities: named entities
+    :param text: text
+    :return: composed by relations
     """
-    return []
+    composition_relations = []
+    return composition_relations
 
 
 def communication_extraction():
@@ -277,16 +377,16 @@ def relations_extraction_heuristic(named_entities, text):
         provided = provided_extraction(relations)
         relations.extend(provided)
     if "DOCUMENT" in ner_labels:
-        definitions = definition_extraction(text)
+        definitions = definition_extraction(named_entities, text)
         relations.extend(definitions)
-    if "STANDARD" in ner_labels:
-        compliances = compliance_extraction()
-        relations.extend(compliances)
+    if "STANDARD" or "CRITERIA" in ner_labels:
+        print("IN COMPLIANCE")
+        compliance = compliance_extraction(named_entities, text)
+        relations.extend(compliance)
     if "COMPONENT" in ner_labels:
         if "SYSTEM" in ner_labels:
             compositions = composition_extraction()
             relations.extend(compositions)
-        print("")
         communications = communication_extraction()
         relations.extend(communications)
         controls = controls_extraction()
@@ -302,20 +402,16 @@ def relations_extraction_heuristic(named_entities, text):
         conditions = conditions_extraction()
         relations.extend(conditions)
     if "UNIT" in ner_labels:
-        print("")
         values = values_extraction()
         relations.extend(values)
         units = units_extraction()
         relations.extend(units)
     if "PROCESS" in ner_labels:
-        print("")
         perform = performances_extraction()
         relations.extend(perform)
         operations = operations_extraction()
         relations.extend(operations)
     if "PHASE" in ner_labels:
-        print("")
         phases = phases_extraction()
         relations.extend(phases)
-    print(text)
     return relations
